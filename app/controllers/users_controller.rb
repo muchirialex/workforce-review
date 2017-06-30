c9c31e2d6869d1a485e64343f3ee6ee236cc6cd1
alex-muchiri <muchiri94@gmail.com>
2017-06-30 12:47:23 +0300

    Initial commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,60 @@
+class UsersController < ApplicationController
+	before_action :find_user, only: [:show, :edit, :update, :destroy]
+	def index
+		if params[:category].blank?
+			@users = User.all.order("created_at DESC").paginate(page: params[:page], per_page: 5)
+		else
+			@category_id = Category.find_by(name: params[:category]).id
+			@users = User.where(category_id: @category_id).order("created_at DESC").paginate(page: params[:page], per_page: 5)
+		end
+	end
+
+	def show
+
+	end
+
+	def create 
+		@user = User.new(users_params)
+
+		if @user.save
+			redirect_to @user
+		else
+			render "new"
+		end
+	end
+
+	def new 
+		@user = User.new
+	end
+
+	def edit
+	end
+
+	def update
+		if @user.update(users_params)
+			redirect_to @user
+		else
+			render "edit"
+		end
+	end
+
+	def destroy
+		@user.destroy
+		redirect_to root_path
+
+	end
+
+	private 
+
+	def users_params
+		params.require(:user).permit(:name, :bio, :category_id, :category_name, :image)
+	end
+
+	def find_user
+		@user = User.find(params[:id])
+	end
+
+
+
+
+end
---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+	belongs_to :category
+
+	has_attached_file :image, styles: { large: "1000x1000#", medium: "550x550#", thumb: "150x150#" }
+	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+end
